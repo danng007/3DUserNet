5df1a97af53336a09439201c0e8650ee676d9db0
Wantao Tang <wantaotang@gmail.com>
2017-11-07 02:36:00 +0000

    write unpack in own version

---
+++ src/examples/ReadE57Files.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wantao on 06/11/17.
+//
+
+#include "ReadE57Files.h"
---
+++ src/examples/ReadE57Files.h
@@ -0,0 +1,14 @@
+//
+// Created by wantao on 06/11/17.
+//
+
+#ifndef E57REFIMPL_READE57FILES_H
+#define E57REFIMPL_READE57FILES_H
+
+
+class ReadE57Files {
+
+};
+
+
+#endif //E57REFIMPL_READE57FILES_H
